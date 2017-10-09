f1a6a2da919767c16f7e4689257d97882af7df7c
Vyas <vramasub@umich.edu>
2017-10-08 23:29:11 -0400

    Remove get_readme function

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 from setuptools import setup, find_packages
 
-# For the long description, if desired can read directly from README.md
-def get_readme():
-    with open('README.md') as f:
-        return f.read()
-
 setup(name='quaternion',
       version='0.1',
       description='Perform quaternion operations using numpy arrays',
-      long_description = get_readme(),
       url='http://github.com/vramasub/quaternion',
       author='Vyas Ramasubramani',
       author_email='vramasub@umich.com',
@@ -22,6 +16,7 @@ setup(name='quaternion',
       classifiers=[
         'Development Status :: 3 - Alpha',
         #'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering :: Mathematics',
       ],
