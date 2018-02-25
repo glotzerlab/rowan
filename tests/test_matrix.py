e8270d8ae67aa5dbe0f5873f3f92775afd7c4de7
Vyas <vramasub@umich.edu>
2018-02-25 12:45:22 -0500

    Additional rotation tests for matrices

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -2,6 +2,7 @@
 
 import unittest
 import numpy as np
+import os
 
 import hamilton as quaternion
 
@@ -9,6 +10,13 @@ zero = np.array([0, 0, 0, 0])
 one = np.array([1, 0, 0, 0])
 half = np.array([0.5, 0.5, 0.5, 0.5])
 
+# Load test files
+TESTDATA_FILENAME = os.path.join(
+    os.path.dirname(__file__),
+    'files/test_arrays.npz')
+with np.load(TESTDATA_FILENAME) as data:
+    input1 = data['input1']
+    vector_inputs = data['vector_inputs']
 
 class TestMatrix(unittest.TestCase):
     """Test rotation matrix conversions"""
@@ -60,3 +68,36 @@ class TestMatrix(unittest.TestCase):
                       [0, 0, -1],
                       [-1, 0, 0]])
         ))
+
+    def test_to_from_matrix(self):
+        # The equality is only guaranteed up to a sign
+        converted = quaternion.from_matrix(
+                        quaternion.to_matrix(
+                        input1))
+        self.assertTrue(
+                np.all(
+                    np.logical_or(
+                        np.isclose(input1 - converted, 0),
+                        np.isclose(input1 + converted, 0),
+                        )
+                    )
+                )
+
+
+    def test_rotation(self):
+        converted = quaternion.from_matrix(
+                        quaternion.to_matrix(
+                        input1))
+
+        quat_rotated = quaternion.rotate(
+                input1,
+                vector_inputs)
+
+        matrices = quaternion.to_matrix(
+                input1)
+        matrix_rotated = np.einsum(
+                'ijk,ki->ij',
+                matrices,
+                vector_inputs.T
+                )
+        self.assertTrue(np.allclose(matrix_rotated, quat_rotated))
