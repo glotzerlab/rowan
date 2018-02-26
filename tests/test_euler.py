7788866a005fa30976dc04bda23ac103135d08f7
Vyas <vramasub@umich.edu>
2018-02-26 11:58:32 -0500

    Add more robust unit tests for euler angles

--- tests/test_euler.py
+++ tests/test_euler.py
@@ -1,20 +1,13 @@
 """Test converting quaternions to and from Euler angles"""
 
-import unittest
 import numpy as np
 import os
 
 import hamilton as quaternion
+import unittest
 
 zero = np.array([0, 0, 0, 0])
 one = np.array([1, 0, 0, 0])
-
-TESTDATA_FILENAME = os.path.join(
-    os.path.dirname(__file__),
-    'files/test_arrays.npz')
-with np.load(TESTDATA_FILENAME) as data:
-    euler_angles = data['euler_angles']
-    euler_quaternions = data['euler_quats']
 
 class TestEuler(unittest.TestCase):
     """Test Euler angle conversions"""
@@ -32,13 +25,6 @@ class TestEuler(unittest.TestCase):
             np.array([0.5, -0.5, 0.5, 0.5])
         ))
 
-        # More complicated test, checks 2d arrays
-        # and more complex angles
-        self.assertTrue(
-                np.allclose(
-                    quaternion.from_euler(euler_angles, 'zyz','intrinsic'),
-                    euler_quaternions
-                    ))
 
     def test_to_euler(self):
         v = one
@@ -50,3 +36,53 @@ class TestEuler(unittest.TestCase):
         self.assertTrue(np.all(
             quaternion.to_euler(v) == np.array([np.pi / 2, 0, np.pi / 2])
         ))
+
+
+    def test_from_to_euler(self):
+        np.random.seed(0)
+        quats = quaternion.normalize(np.random.rand(25, 4))
+        angles = quaternion.normalize(np.random.rand(25, 3))
+        conventions = ['xzx', 'xyx', 'yxy', 'yzy', 'zyz', 'zxz',
+                'xzy', 'xyz', 'yxz', 'yzx', 'zyx', 'zxy']
+        axis_types = ['extrinsic', 'intrinsic']
+
+        # Test one direction
+        for convention in conventions:
+            for axis_type in axis_types:
+                out = quaternion.from_euler(
+                    quaternion.to_euler(quats, convention, axis_type),
+                    convention, axis_type
+                    )
+                self.assertTrue(
+                    np.all(
+                        np.logical_or(
+                            np.isclose(out - quats, 0),
+                            np.isclose(out + quats, 0)
+                            )
+                        ),
+                    msg="Failed for convention {}, axis type {}".format(
+                        convention, axis_type))
+
+    def test_to_from_euler(self):
+        np.random.seed(0)
+        quats = quaternion.normalize(np.random.rand(25, 4))
+        angles = quaternion.normalize(np.random.rand(25, 3))
+        conventions = ['xzx', 'xyx', 'yxy', 'yzy', 'zyz', 'zxz',
+                'xzy', 'xyz', 'yxz', 'yzx', 'zyx', 'zxy']
+        axis_types = ['extrinsic', 'intrinsic']
+
+        for convention in conventions:
+            for axis_type in axis_types:
+                out = quaternion.to_euler(
+                    quaternion.from_euler(angles, convention, axis_type),
+                    convention, axis_type
+                    )
+                self.assertTrue(
+                    np.all(
+                        np.logical_or(
+                            np.isclose(out - angles, 0),
+                            np.isclose(out + angles, 0)
+                            )
+                        ),
+                    msg="Failed for convention {}, axis type {}".format(
+                        convention, axis_type))
