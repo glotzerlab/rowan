5e46ce90d0126dd84ff6b13167a9806909c2f256
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2018-03-02 17:57:21 -0500

    Add basic coverage support, add missing test case

---
+++ tests/test_vector_vector.py
@@ -0,0 +1,16 @@
+"""Test function to rotate vector onto vector"""
+from __future__ import division, print_function, absolute_import
+
+import unittest
+import numpy as np
+
+import hamilton as quaternion
+
+
+class TestVectorVector(unittest.TestCase):
+    def test_vector_vector_rotation(self):
+        """Test finding quaternion to rotate a vector onto another vector"""
+        vec1 = np.array([0, 0, 1])
+        vec2 = np.array([1, 0, 0])
+        quat = quaternion.vector_vector_rotation(vec1, vec2)
+        self.assertTrue(np.allclose(quat, np.array([[0, np.sqrt(2)/2, 0, np.sqrt(2)/2]])))
