db3f4e29af1cf8569424972396faa15e9f20ae00
Vyas <vramasub@umich.edu>
2018-03-11 17:50:52 -0400

    Fix __all__

--- hamilton/__init__.py
+++ hamilton/__init__.py
@@ -17,17 +17,17 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                        '_version.py')) as f:
     exec(f.read())
 
-__all__ = [conjugate,
-           multiply,
-           norm,
-           normalize,
-           rotate,
-           about_axis,
-           vector_vector_rotation,
-           from_euler,
-           to_euler,
-           from_matrix,
-           to_matrix,
-           from_axis_angle,
-           to_axis_angle
+__all__ = ['conjugate',
+           'multiply',
+           'norm',
+           'normalize',
+           'rotate',
+           'about_axis',
+           'vector_vector_rotation',
+           'from_euler',
+           'to_euler',
+           'from_matrix',
+           'to_matrix',
+           'from_axis_angle',
+           'to_axis_angle'
            ]
