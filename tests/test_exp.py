9cf1577d93c174a453cae59ad8308d851d2a0006
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2018-04-06 10:56:39 -0400

    Fix path

--- tests/test_exp.py
+++ tests/test_exp.py
@@ -2,6 +2,7 @@
 from __future__ import division, print_function, absolute_import
 
 import numpy as np
+import os
 
 import hamilton as quaternion
 import unittest
@@ -14,7 +15,9 @@ class TestExp(unittest.TestCase):
         """Ensure that quaternion exponential behaves correctly"""
         np.random.seed(0)
         shapes = [(4,), (1, 4), (3, 4, 4), (12, 7, 3, 4)]
-        answers = np.load('files/test_exp.npz')
+        answers = np.load(os.path.join(
+            os.path.dirname(__file__),
+            'files/test_exp.npz'))
         for shape in shapes:
             x = np.random.random_sample(shape)
             self.assertTrue(
