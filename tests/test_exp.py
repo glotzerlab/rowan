317787346783132b4b479aff03e23ae9a14b0554
Vyas <vramasub@umich.edu>
2018-04-07 13:21:54 -0400

    Flake8

--- tests/test_exp.py
+++ tests/test_exp.py
@@ -10,6 +10,7 @@ import unittest
 one = np.array([1, 0, 0, 0])
 zero = np.array([0, 0, 0, 0])
 
+
 class TestExp(unittest.TestCase):
     """Test exponential function"""
 
@@ -17,14 +18,14 @@ class TestExp(unittest.TestCase):
         """Ensure that quaternion exponential behaves correctly"""
         self.assertTrue(
                 np.all(quaternion.exp(one) ==
-                    np.array([np.exp(1), 0, 0, 0])
-                    )
+                       np.array([np.exp(1), 0, 0, 0])
+                       )
                 )
         x = np.array([0, 1, 1, 1])
         self.assertTrue(
                 np.allclose(
                     quaternion.exp(x),
-                    np.array([-0.16055654, 0.5698601, 0.5698601, 0.5698601 ])
+                    np.array([-0.16055654, 0.5698601, 0.5698601, 0.5698601])
                     )
                 )
         self.assertTrue(np.all(quaternion.exp(zero) == one))
@@ -47,7 +48,12 @@ class TestExp(unittest.TestCase):
         x = np.array([1, 0, 0, 0])
         self.assertTrue(np.all(quaternion.log(x) == zero))
         x = np.array([0, 0, 0, 0])
-        self.assertTrue(np.all(quaternion.log(x) == np.array([-np.inf, 0, 0, 0])))
+        self.assertTrue(
+                np.all(
+                    quaternion.log(x) ==
+                    np.array([-np.inf, 0, 0, 0])
+                    )
+                )
 
         np.random.seed(0)
         shapes = [(4,), (1, 4), (3, 4, 4), (12, 7, 3, 4)]
