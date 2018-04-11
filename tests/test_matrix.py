bf612ff974ef90647a5e745c04df2592d828cc33
Vyas <vramasub@umich.edu>
2018-04-11 11:22:58 -0400

    Change all tests to use package name directly

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -5,7 +5,7 @@ import unittest
 import numpy as np
 import os
 
-import rowan as quaternion
+import rowan
 
 zero = np.array([0, 0, 0, 0])
 one = np.array([1, 0, 0, 0])
@@ -26,12 +26,12 @@ class TestMatrix(unittest.TestCase):
     def test_from_matrix(self):
 
         self.assertTrue(np.all(
-            quaternion.from_matrix(np.eye(3)) == one
+            rowan.from_matrix(np.eye(3)) == one
         ))
 
         with self.assertRaises(ValueError):
             self.assertTrue(np.allclose(
-                quaternion.from_matrix(
+                rowan.from_matrix(
                     2*np.eye(3)
                     )
             ))
@@ -40,7 +40,7 @@ class TestMatrix(unittest.TestCase):
                         [1, 0, 0],
                         [0, 1, 0]])
         self.assertTrue(np.allclose(
-            quaternion.from_matrix(mat), half
+            rowan.from_matrix(mat), half
         ))
 
         mat = np.array([[0, 1, 0],
@@ -49,26 +49,26 @@ class TestMatrix(unittest.TestCase):
         v = np.copy(half)
         v[3] *= -1
         self.assertTrue(np.allclose(
-            quaternion.from_matrix(mat), v
+            rowan.from_matrix(mat), v
         ))
 
     def test_to_matrix(self):
         v = np.copy(zero)
         with self.assertRaises(ZeroDivisionError):
-            quaternion.to_matrix(v)
+            rowan.to_matrix(v)
 
         v = 2*np.ones(4)
         with self.assertRaises(ValueError):
-            quaternion.to_matrix(v)
+            rowan.to_matrix(v)
 
         v = np.copy(one)
         self.assertTrue(np.all(
-            quaternion.to_matrix(v) == np.eye(3)
+            rowan.to_matrix(v) == np.eye(3)
         ))
 
         v = np.copy(half)
         self.assertTrue(np.allclose(
-            quaternion.to_matrix(v),
+            rowan.to_matrix(v),
             np.array([[0, 0, 1],
                       [1, 0, 0],
                       [0, 1, 0]])
@@ -76,7 +76,7 @@ class TestMatrix(unittest.TestCase):
 
         v[3] *= -1
         self.assertTrue(np.allclose(
-            quaternion.to_matrix(v),
+            rowan.to_matrix(v),
             np.array([[0, 1, 0],
                       [0, 0, -1],
                       [-1, 0, 0]])
@@ -84,8 +84,8 @@ class TestMatrix(unittest.TestCase):
 
     def test_to_from_matrix(self):
         # The equality is only guaranteed up to a sign
-        converted = quaternion.from_matrix(
-            quaternion.to_matrix(
+        converted = rowan.from_matrix(
+            rowan.to_matrix(
                 input1))
         self.assertTrue(
             np.all(
@@ -97,11 +97,11 @@ class TestMatrix(unittest.TestCase):
         )
 
     def test_rotation(self):
-        quat_rotated = quaternion.rotate(
+        quat_rotated = rowan.rotate(
             input1,
             vector_inputs)
 
-        matrices = quaternion.to_matrix(
+        matrices = rowan.to_matrix(
             input1)
         matrix_rotated = np.einsum(
             'ijk,ki->ij',
