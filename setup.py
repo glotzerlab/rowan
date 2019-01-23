08a769147e4aa8ac14f4d2fc48db25474a62ab31
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2019-01-23 17:51:41 -0500

    Bump up to version 1.1.7.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import os
 
 # Gets the version
-version = '1.1.6'
+version = '1.1.7'
 
 # Read README for PyPI, fallback if it fails.
 desc = 'Perform quaternion operations using NumPy arrays'
