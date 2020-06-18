7b60a3ba5c835e1b3e9fe33342f5f6a5a14a74fa
Vyas Ramasubramani <vramasub@umich.edu>
2020-06-18 14:40:48 -0400

    Fix style issues with doctest tests.

--- tests/test_docstrings.py
+++ tests/test_docstrings.py
@@ -1,3 +1,4 @@
+"""Test examples in code docstrings using doctest."""
 import doctest
 import pkgutil
 import unittest
@@ -6,11 +7,12 @@ import rowan
 
 
 def load_tests(loader, tests, ignore):
-    modules = pkgutil.walk_packages(rowan.__path__, rowan.__name__ + '.')
+    """Create tests from all docstrings by walking the package hierarchy."""
+    modules = pkgutil.walk_packages(rowan.__path__, rowan.__name__ + ".")
     for _, module_name, _ in modules:
-        tests.addTests(doctest.DocTestSuite(module_name, globs={'rowan': rowan}))
+        tests.addTests(doctest.DocTestSuite(module_name, globs={"rowan": rowan}))
     return tests
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
